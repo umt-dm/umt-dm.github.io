773d53d1b81cd392bf5ba6317025c9fe789e2cf7
MAbdullahAhmad <theneweratech@gmail.com>
2024-10-24 00:51:12 +0500

    added favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+        <title>UMT DM | Project Career Guide</title>
+      </Head>
       <body>
         <Main />
         <NextScript />
