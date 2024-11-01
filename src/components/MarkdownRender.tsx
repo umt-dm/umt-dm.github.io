5b0f8ed7fac2d04169bbe431518a57a95ea3557e
MAbdullahAhmad <theneweratech@gmail.com>
2024-11-01 23:09:20 +0500

    added props to md tables

--- src/components/MarkdownRender.tsx
+++ src/components/MarkdownRender.tsx
@@ -138,25 +138,25 @@ const MarkdownRender: React.FC<CustomMarkdownProps> = ({ content }) => {
       </Button>
     ),
 
-    table: ({ children }: any) => (
+    table: ({ children, ...rest }: any) => (
       <Box sx={{ overflowX: 'auto', mb: 2 }}>
-        <table style={{ width: '100%', borderCollapse: 'collapse' }}>
+        <table style={{ width: '100%', borderCollapse: 'collapse' }} {...rest}>
           {children}
         </table>
       </Box>
     ),
-    thead: ({ children }: any) => (
-      <thead style={{ backgroundColor: 'grey.800' }}>
+    thead: ({ children, ...rest }: any) => (
+      <thead style={{ backgroundColor: 'grey.800' }} {...rest}>
         {children}
       </thead>
     ),
-    tbody: ({ children }: any) => (
-      <tbody style={{ backgroundColor: 'grey.700' }}>{children}</tbody>
+    tbody: ({ children, ...rest }: any) => (
+      <tbody style={{ backgroundColor: 'grey.700' }} {...rest}>{children}</tbody>
     ),
-    tr: ({ children }: any) => (
-      <tr style={{ borderBottom: '1px solid transparent' }}>{children}</tr>
+    tr: ({ children, ...rest }: any) => (
+      <tr style={{ borderBottom: '1px solid transparent' }} {...rest}>{children}</tr>
     ),
-    th: ({ children }: any) => (
+    th: ({ children, ...rest }: any) => (
       <th 
         style={{
           padding: '8px', 
@@ -164,17 +164,19 @@ const MarkdownRender: React.FC<CustomMarkdownProps> = ({ content }) => {
           textAlign: 'left', 
           borderBottom: '1px solid white', // White border for headers
         }}
+        {...rest}
       >
         {children}
       </th>
     ),
-    td: ({ children }: any) => (
+    td: ({ children, ...rest }: any) => (
       <td 
         style={{
           padding: '8px', 
           color: 'white', 
           borderBottom: '1px solid rgba(255, 255, 255, 0.1)',
         }}
+        {...rest}
       >
         {children}
       </td>
